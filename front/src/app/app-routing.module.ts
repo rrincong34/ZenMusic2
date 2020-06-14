668350b3f95f6ebcd1f865b81be717bebd6ead2d
Richard <rrincong@gmail.com>
2020-06-14 13:48:24 -0500

    Actualizacion front

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -2,22 +2,26 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 // Importar los componentes de navegación
+
+import { HomeComponent } from './components/home/home.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { LoginComponent } from './components/login/login.component';
 import { PerfilUsuarioComponent } from './components/perfil-usuario/perfil-usuario.component';
 import { CrearPlayListComponent } from './components/crear-play-list/crear-play-list.component';
 import { MenuNoRegistradosComponent } from './components/menu-no-registrados/menu-no-registrados.component';
-import { HomeComponent } from './components/home/home.component';
+
 
 
 // Relacionar rutas con componentes
 const routes: Routes = [
-    {path: '', component: RegistroComponent},
+    {path: 'registro', component: RegistroComponent},
     {path: 'login', component: LoginComponent},
     {path: 'perfil', component: PerfilUsuarioComponent},
     {path: 'crear-play-list', component: CrearPlayListComponent},
     {path: 'menu-no-registrados', component: MenuNoRegistradosComponent},
-    {path: 'home', component: HomeComponent}
+    {path: '', component: HomeComponent}
+
+    
 
 ];
 
