ee3aa22b21d0d7f01917d0b1bf498f1f0f154dfd
Angietellez <angie.a.tellez87@gmail.com>
2020-06-17 12:07:13 -0500

    pagina de reproduccion

--- front/src/app/components/cancion-todas/cancion-todas.component.ts
+++ front/src/app/components/cancion-todas/cancion-todas.component.ts
@@ -7,103 +7,107 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CancionTodasComponent implements OnInit {
   aguabajo_img = [ 'assets/ImgCanciones/Segun_la_CNN-Chirimoya.jpg'];
-  alOido_img = [ 'assets/ImgCanciones/monica.jpg'];
+  alOido_img = [ 'assets/ImgCanciones/oi.jpg'];
   anzar_img = [ 'assets/ImgCanciones/anzar-Tusho.jpg'];
   bajoTierra_img = [ 'assets/ImgCanciones/tierra.jpg'];
   caballito_img = [ 'assets/ImgCanciones/Caballito_de_raquira-niyireth.jpg'];
   cancionAgradecimiento_img = [ 'assets/ImgCanciones/Cancion_de_Agradecimiento-Silbidos.jpg'];
   dejateLlevar_img = [ 'assets/ImgCanciones/Dejate_llevar-Art_Bembe.jpg'];
   feliz_img = [ 'assets/ImgCanciones/Dejate_llevar-Art_Bembe.jpg'];
-  paQueTrabajar_img = [ 'assets/ImgCanciones/cecilia.jpg'];
-  chocolate_img = [ 'assets/ImgCanciones/profetas.jpg'];
+  paQueTrabajar_img = [ 'assets/ImgCanciones/la.jpg'];
+  chocolate_img = [ 'assets/ImgCanciones/cho.jpg'];
   remanso_img = [ 'assets/ImgCanciones/chonta.jpg'];
   solo_img = [ 'assets/ImgCanciones/Solo-Ekhymosis.jpg'];
-  tuBoca_img = [ 'assets/ImgCanciones/Tu_boca-Cabas.jpg'];
-  teInvito_img = [ 'assets/ImgCanciones/herencia.jpg'];
+  tuBoca_img = [ 'assets/ImgCanciones/cab.jpg'];
+  teInvito_img = [ 'assets/ImgCanciones/te.jpg'];
 
 
 cancion = {
   aguabajo_Nombre: 'SEGUN LA CNN',
   alOido_Nombre: 'AL OIDO',
   anzar_Nombre: 'ANZAR',
-  bajoTierra_Nombre: 'SUPER HOLGAZAN',
+  bajoTierra_Nombre: 'BAJO TIERRA',
   caballito_Nombre: 'CABALLITO DE RAQUIRA',
-  cancionAgradecimiento_Nombre: 'SUPER HSEQ',
-  dejateLlevar_Nombre: 'SUPER HOLGAZAN',
-  feliz_Nombre: 'SUPER HSEQ',
-  paQueTrabajar_Nombre: 'SUPER HOLGAZAN',
-  chocolate_Nombre: 'SUPER HSEQ',
-  remanso_Nombre: 'SUPER HOLGAZAN',
-  solo_Nombre: 'SUPER HOLGAZAN',
-  tuBoca_Nombre: 'SUPER HSEQ',
-  teInvito_Nombre: 'SUPER HOLGAZAN'
+  cancionAgradecimiento_Nombre: 'CANCION DE AGRADECIMIENTO',
+  dejateLlevar_Nombre: 'DEJATE LLEVAR',
+  feliz_Nombre: 'SE FELIZ',
+  paQueTrabajar_Nombre: 'PA QUE TRABAJAR',
+  chocolate_Nombre: 'CHOCOLATE',
+  remanso_Nombre: 'REMANSO INICIAL - PURA CHONTA',
+  solo_Nombre: 'SOLO',
+  tuBoca_Nombre: 'TU BOCA',
+  teInvito_Nombre: 'TE INVITO'
 }
 
 
 album = {
   aguabajo_album: 'Chirimoya',
   alOido_album: 'Al Oído The best of Mónica Giraldo',
-  anzar_album: '',
-  bajoTierra_album: 'SUPER HOLGAZAN',
+  anzar_album: 'Anzar',
+  bajoTierra_album: 'Lavandería real',
   caballito_album: 'Music from Colombia',
-  cancionAgradecimiento_album: 'SUPER HSEQ',
-  dejateLlevar_album: 'SUPER HOLGAZAN',
-  feliz_album: 'SUPER HSEQ',
-  paQueTrabajar_album: 'SUPER HOLGAZAN',
-  chocolate_album: 'SUPER HSEQ',
-  remanso_album: 'SUPER HOLGAZAN',
-  solo_album: 'SUPER HOLGAZAN',
-  tuBoca_album: 'SUPER HSEQ',
-  teInvito_album: 'SUPER HOLGAZAN'
+  cancionAgradecimiento_album: 'Silbidos',
+  dejateLlevar_album: 'Art Bembé',
+  feliz_album: 'Art Bembé',
+  paQueTrabajar_album: 'Buenaventura',
+  chocolate_album: 'Baila',
+  remanso_album: 'Pura chonta',
+  solo_album: 'Niño gigante',
+  tuBoca_album: 'Cabas',
+  teInvito_album: 'Tambó'
 }
 
 
 emocion = {
-  emocion1: '',
-  emocion2: '',
-  emocion3: '',
-  emocion4: '',
-  emocion5: ''
+  emocion1: 'Amor',
+  emocion2: 'Alegria',
+  emocion3: 'Tranquilidad',
+  emocion4: 'Pasion',
+  emocion5: 'Gratitud'
 }
 
 
 genero = {
   genero1: 'Folclorico',
   genero2: 'Música colombiana',
-  genero3: '',
-  genero4: '',
-  genero5: '',
-  genero6: '',
-  genero7: ''
+  genero3: 'Romantica',
+  genero4: 'Alternativa/independiente',
+  genero5: 'Pop',
+  genero6: 'música colombiana/reggae/hip hop',
+  genero7: 'Folclor',
+  genero8: 'Rock en español',
+  genero9: 'Salsa/música tropical'
 }
 
 artista = {
   aguabajo_artista: 'Aguabajo',
   alOido_artista: 'Mónica Giraldo',
   anzar_artista: 'Tusho Medina',
-  bajoTierra_artista: 'SUPER HOLGAZAN',
+  bajoTierra_artista: 'Bajo Tierra',
   caballito_artista: 'Niyireth Alarcon',
-  cancionAgradecimiento_artista: 'SUPER HSEQ',
-  dejateLlevar_artista: 'SUPER HOLGAZAN',
-  feliz_artista: 'SUPER HSEQ',
-  paQueTrabajar_artista: 'SUPER HOLGAZAN',
-  chocolate_artista: 'SUPER HSEQ',
-  remanso_artista: 'SUPER HOLGAZAN',
-  solo_artista: 'SUPER HOLGAZAN',
-  tuBoca_artista: 'SUPER HSEQ',
-  teInvito_artista: 'SUPER HOLGAZAN'
+  cancionAgradecimiento_artista: 'Las Añez',
+  dejateLlevar_artista: 'Gema y Pável',
+  feliz_artista: 'Gema y Pável',
+  paQueTrabajar_artista: 'La Santa Cecilia',
+  chocolate_artista: 'Profetas',
+  remanso_artista: 'Grupo Bahia',
+  solo_artista: 'Ekhymosis',
+  tuBoca_artista: 'Andrés Cabas',
+  teInvito_artista: 'Herencia de Timbiquí'
 }
 
 
-año = {
-  genero1: '2014',
-  genero2: '2019',
-  genero3: '2013',
-  genero4: '',
-  genero5: '2016',
-  genero6: '2014',
-  genero7: '2019',
-  genero8: '2013'
+anio = {
+  anio1993: '1993',
+  anio1996: '1996',
+  anio1997: '1997',
+  anio2002: '2002',
+  anio2011: '2011',
+  anio2013: '2013',
+  anio2014: '2014',
+  anio2015: '2015',
+  anio2016: '2016',
+  anio2019: '2019'
 }
 
 constructor() { }
